f05b42765133479f608a9c37380715e1455202e8
AVlyx <132539818+AVlyx@users.noreply.github.com>
2025-04-14 11:24:07 +0200

    Update src/multilinear.jl
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>

--- src/multilinear.jl
+++ src/multilinear.jl
@@ -504,7 +504,7 @@ export apply_to_subsystem
     ρ::AbstractMatrix,
     ssys::Integer,
     dims::AbstractVector = _equal_sizes(ρ)
-Apply the operator op on the subsytems of ρ identified by ssys
+Apply the operator `op` on the subsytems of `ρ` identified by `ssys`
 If the argument `dims` is omitted two equally-sized subsystems are assumed.
 """
 apply_to_subsystem(
