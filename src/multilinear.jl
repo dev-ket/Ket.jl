58886b78112f3414d67671bfdb8369ee9564f207
AVlyx <132539818+AVlyx@users.noreply.github.com>
2025-05-04 17:37:09 +0200

    apply to subsystem (#20)
    * apply to ssys
    
    * type stable apply to ssys
    
    * fix tests
    
    * Apply to ssys (#2)
    
    * apply to ssys not tested
    
    * added tests
    
    * added sparse arrays optimization
    
    * rm comments
    
    * runtests file
    
    * rm testfile
    
    * apply to ssys
    
    * not contiguous, not square => throw exception
    
    * tiny cleanup
    
    * docs
    
    * assert => throw
    
    * Update src/multilinear.jl
    
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>
    
    * rename ctg_ssys => contiguous_subsysystem
    
    * contiguous_subsytem redundant check
    
    * sparse, fix when output size is 1
    
    * Update src/multilinear.jl
    
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>
    
    * Update src/multilinear.jl
    
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>
    
    * Update src/multilinear.jl
    
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>
    
    * apply to subsytem on matrices of different types
    
    * edge case, input_size == 1
    
    * fix for sparse JuMP variables
    
    * remove redundant tests
    
    * removed completed TODO
    
    * permute systems tests
    
    ---------
    
    Co-authored-by: Mateus Araújo <maltusan@gmail.com>

--- src/multilinear.jl
+++ src/multilinear.jl
@@ -179,8 +179,7 @@ for (T, wrapper) ∈ [(:AbstractMatrix, :identity), (:(Hermitian), :(Hermitian))
             perm = vcat(keep, transp)
             dims_perm = vcat(dims_keep, dims_transp)
 
-            p = sortperm(perm)
-            inv_perm = collect(1:nsys)[p]
+            inv_perm = sortperm(perm)
             X_perm = permute_systems(X, perm, dims)
 
             @inbounds for j ∈ 1:transp_size:X_size-1, i ∈ 1:transp_size:X_size-1
@@ -362,3 +361,245 @@ If the argument `dims` is omitted two equally-sized subsystems are assumed.
 """
 trace_replace(X::AbstractMatrix, remove::Integer, dims::AbstractVector{<:Integer} = _equal_sizes(X)) =
     trace_replace(X, [remove], dims)
+
+"""
+apply_to_subsystem(
+op::AbstractMatrix,
+ψ::AbstractVector,
+ssys::AbstractVector,
+dims::AbstractVector = _equal_sizes(ρ)
+Apply the operator `op` on the subsytems of `ρ` identified by `ssys`
+(op ⊗ I) * ψ
+If the argument `dims` is omitted two equally-sized subsystems are assumed.
+"""
+function apply_to_subsystem(
+    op::AbstractMatrix,
+    ψ::AbstractVector,
+    ssys::AbstractVector{<:Integer},
+    dims::AbstractVector{<:Integer} = _equal_sizes(ψ)
+)
+    isempty(ssys) && throw(ArgumentError("ssys vector must not be empty"))
+    prod(dims) == length(ψ) || throw(DimensionMismatch("ψ has length $(length(ψ)), expected length $(prod(dims))"))
+    prod(dims[ssys]) == size(op, 2) ||
+        throw(DimensionMismatch("op has dimensions $(size(op)), expected $((size(op,1), prod(dims[ssys])))"))
+    square_op = size(op, 1) == size(op, 2)
+    contiguous_subsystems = ssys == ssys[1]:ssys[end]
+    if !contiguous_subsystems && !square_op
+        throw(ArgumentError("op needs to be square or ssys need to be contiguous and ordered"))
+    end
+
+    nsys = length(dims)
+    keep = _subsystems_complement(ssys, nsys)
+    dims_keep = dims[keep]
+
+    ψ_length = length(ψ)
+    input_size = size(op, 2)
+    output_size = size(op, 1)
+    keep_size = prod(dims_keep)
+    Y_length = keep_size * output_size
+
+    perm = vcat(keep, ssys)
+    ψ_perm = permute_systems(ψ, perm, dims)
+
+    Y_type = Base.promote_op(*, eltype(op), eltype(ψ))
+    Y = Vector{Y_type}(undef, Y_length) #hack for JuMP variables
+
+    if eltype(ψ) <: JuMP.AbstractJuMPScalar
+        for (i_in, i_out) ∈ zip(1:input_size:1+ψ_length-input_size, 1:output_size:1+Y_length-output_size)
+            @views Y[i_out:i_out+output_size-1] .= op * ψ_perm[i_in:i_in+input_size-1]
+        end
+    else
+        for (i_in, i_out) ∈ zip(1:input_size:1+ψ_length-input_size, 1:output_size:1+Y_length-output_size)
+            @views mul!(Y[i_out:i_out+output_size-1], op, ψ_perm[i_in:i_in+input_size-1])
+        end
+    end
+
+    inv_perm = sortperm(perm)
+    output_dims = contiguous_subsystems ? vcat(ones(eltype(dims), length(ssys) - 1), [output_size]) : dims[ssys] # either contiguous ssys or square operators
+    dims_perm_output = vcat(dims_keep, output_dims) # The dims of the subsystem when applying the inverse permutation
+    return permute_systems(Y, inv_perm, dims_perm_output)
+end
+
+export apply_to_subsystem
+
+"""
+apply_to_subsystem(
+op::AbstractMatrix,
+ψ::AbstractVector,
+ssys::Integer,
+dims::AbstractVector = _equal_sizes(ρ)
+Apply the operator `op` on the subsytems of `ρ` identified by `ssys`
+(op ⊗ I) * ψ
+If the argument `dims` is omitted two equally-sized subsystems are assumed.
+"""
+apply_to_subsystem(
+    op::AbstractMatrix,
+    ψ::AbstractVector,
+    ssys::Integer,
+    dims::AbstractVector{<:Integer} = _equal_sizes(ψ)
+) = apply_to_subsystem(op, ψ, [ssys], dims)
+
+"""
+apply_to_subsystem(
+kraus::AbstractVector{<:AbstractMatrix},
+ρ::AbstractMatrix,
+ssys::AbstractVector,
+dims::AbstractVector = _equal_sizes(ρ)
+Apply the operators `kraus` on the subsytems of `ρ` identified by `ssys`
+∑(Kᵢ ⊗ I) * ρ * (Kᵢ ⊗ I)†
+For each input subsystem `i` of size `dims[ssys[i]]` they are mapped to a subsystem of size `output_dims[i]`
+If the argument `dims` is omitted two equally-sized subsystems are assumed.
+"""
+function apply_to_subsystem(
+    kraus::AbstractVector{<:AbstractMatrix},
+    ρ::AbstractMatrix,
+    ssys::AbstractVector{<:Integer},
+    dims::AbstractVector{<:Integer} = _equal_sizes(ρ)
+)
+    isempty(ssys) && throw(ArgumentError("ssys vector must not be empty"))
+    square_kraus_ops = all([size(k, 1) == size(k, 2) for k ∈ kraus])
+    contiguous_subsystems = ssys == ssys[1]:ssys[end]
+    if (!contiguous_subsystems && !square_kraus_ops)
+        throw(ArgumentError("Kraus operator need to be square or ssys need to be contiguous and ordered"))
+    end
+    nsys = length(dims)
+    keep = _subsystems_complement(ssys, nsys)
+
+    dims_keep = dims[keep]
+    input_dims = dims[ssys]
+
+    input_size = prod(input_dims) #input size of Kraus
+    output_size = size(kraus[1], 1)
+    keep_size = prod(dims_keep)
+    ρ_size = prod(dims)
+    Y_size = keep_size * output_size
+
+    if (ρ_size, ρ_size) != size(ρ)
+        throw(DimensionMismatch("ρ has dimensions $(size(ρ)), expected dimensions $((ρ_size, ρ_size))"))
+    end
+    if !all([size(k, 2) == input_size for k ∈ kraus]) || !all([size(k, 1) == output_size for k ∈ kraus])
+        throw(DimensionMismatch("Kraus operators have invalid dimensions"))
+    end
+
+    perm = vcat(keep, ssys)
+    ρ_perm = permute_systems(ρ, perm, dims)
+
+    kraus_type = promote_type([eltype(k) for k ∈ kraus]...)
+    Y_type = Base.promote_op(*, kraus_type, eltype(ρ))
+    Y = Matrix{Y_type}(undef, Y_size, Y_size) #hack for JuMP variables
+    for i ∈ eachindex(Y) # Init with 0s
+        Y[i] = 0
+    end
+
+    if eltype(ρ) <: JuMP.AbstractJuMPScalar
+        for (j_in, j_out) ∈ zip(1:input_size:1+ρ_size-input_size, 1:output_size:Y_size-output_size+1),
+            (i_in, i_out) ∈ zip(1:input_size:1+ρ_size-input_size, 1:output_size:Y_size-output_size+1)
+
+            for kraus_op ∈ kraus
+                @views Y[i_out:i_out+output_size-1, j_out:j_out+output_size-1] .+=
+                    kraus_op * ρ_perm[i_in:i_in+input_size-1, j_in:j_in+input_size-1] * kraus_op'
+            end
+        end
+    else
+        interm = Matrix{Y_type}(undef, size(kraus[1]))
+        for (j_in, j_out) ∈ zip(1:input_size:1+ρ_size-input_size, 1:output_size:Y_size-output_size+1),
+            (i_in, i_out) ∈ zip(1:input_size:1+ρ_size-input_size, 1:output_size:Y_size-output_size+1)
+
+            for kraus_op ∈ kraus
+                @views mul!(interm, kraus_op, ρ_perm[i_in:i_in+input_size-1, j_in:j_in+input_size-1])
+                @views mul!(Y[i_out:i_out+output_size-1, j_out:j_out+output_size-1], interm, kraus_op', true, true)
+            end
+        end
+    end
+
+    inv_perm = sortperm(perm)
+    output_dims = contiguous_subsystems ? vcat(ones(eltype(dims), length(ssys) - 1), [output_size]) : dims[ssys] # either contiguous ssys or square operators
+    dims_perm_output = vcat(dims_keep, output_dims) # The dims of the subsystem when applying the inverse permutation
+    return permute_systems(Y, inv_perm, dims_perm_output)
+end
+
+"""
+apply_to_subsystem(
+kraus::AbstractVector{<:AbstractMatrix},
+ρ::AbstractMatrix,
+ssys::AbstractVector,
+dims::AbstractVector = _equal_sizes(ρ)
+Apply the operators `kraus` on the subsytems of `ρ` identified by `ssys`
+∑(Kᵢ ⊗ I) * ρ * (Kᵢ ⊗ I)†
+If the argument `dims` is omitted two equally-sized subsystems are assumed.
+"""
+function apply_to_subsystem(
+    kraus::AbstractVector{<:SA.AbstractSparseArray},
+    ρ::SA.AbstractSparseArray,
+    ssys::AbstractVector{<:Integer},
+    dims::AbstractVector{<:Integer} = _equal_sizes(ρ)
+)
+    isempty(ssys) && throw(ArgumentError("ssys vector must not be empty"))
+    square_kraus_ops = all([size(k, 1) == size(k, 2) for k ∈ kraus])
+    contiguous_subsystems = ssys == ssys[1]:ssys[end]
+    if (!contiguous_subsystems && !square_kraus_ops)
+        throw(ArgumentError("Kraus operator need to be square or ssys need to be contiguous and ordered"))
+    end
+    nsys = length(dims)
+    keep = _subsystems_complement(ssys, nsys)
+
+    dims_keep = dims[keep]
+    input_dims = dims[ssys]
+
+    input_size = prod(input_dims) #input size of Kraus
+    output_size = size(kraus[1], 1)
+    keep_size = prod(dims_keep)
+    ρ_size = prod(dims)
+    Y_size = keep_size * output_size
+
+    if (ρ_size, ρ_size) != size(ρ)
+        throw(DimensionMismatch("ρ has dimensions $(size(ρ)), expected dimensions $((ρ_size, ρ_size))"))
+    end
+    if !all([size(k, 2) == input_size for k ∈ kraus]) || !all([size(k, 1) == output_size for k ∈ kraus])
+        throw(DimensionMismatch("Kraus operators have invalid dimensions"))
+    end
+
+    perm = vcat(keep, ssys)
+    ρ_perm = permute_systems(ρ, perm, dims)
+
+    kraus_type = promote_type([eltype(k) for k ∈ kraus]...)
+    Y_type = Base.promote_op(*, kraus_type, eltype(ρ))
+    Y = SA.spzeros(Y_type, Y_size, Y_size)
+    spI = SA.sparse(I, keep_size, keep_size)
+    if eltype(ρ) <: JuMP.AbstractJuMPScalar
+        for k ∈ kraus
+            k_kron = kron(spI, k)
+            Y .+= k_kron * ρ_perm * k_kron'
+        end
+    else
+        temp = SA.spzeros(Y_type, output_size * keep_size, input_size * keep_size)
+        for k ∈ kraus
+            k_kron = kron(spI, k)
+            mul!(temp, k_kron, ρ_perm)
+            mul!(Y, temp, k_kron', true, true)
+        end
+    end
+
+    inv_perm = sortperm(perm)
+    output_dims = contiguous_subsystems ? vcat(ones(eltype(dims), length(ssys) - 1), [output_size]) : dims[ssys] # either contiguous ssys or square operators
+    dims_perm_output = vcat(dims_keep, output_dims) # The dims of the subsystem when applying the inverse permutation
+    return permute_systems(Y, inv_perm, dims_perm_output)
+end
+"""
+apply_to_subsystem(
+kraus::AbstractVector{<:AbstractMatrix},
+ρ::AbstractMatrix,
+ssys::Integer,
+dims::AbstractVector = _equal_sizes(ρ)
+Apply the operators `kraus` on the subsytem of `ρ` identified by `ssys`
+∑(Kᵢ ⊗ I) * ρ * (Kᵢ ⊗ I)†
+The subsystem identified by `ssys` of size `dims[ssys]` is mapped to a subsystem of size `output_dims[ssys]`
+"""
+function apply_to_subsystem(
+    kraus::AbstractVector{<:AbstractMatrix},
+    ρ::AbstractMatrix,
+    ssys::Integer,
+    dims::AbstractVector{<:Integer} = _equal_sizes(ρ)
+)
+    apply_to_subsystem(kraus, ρ, [ssys], dims)
+end
--- test/multilinear.jl
+++ test/multilinear.jl
@@ -1,4 +1,228 @@
 @testset "Multilinear algebra   " begin
+    @testset "Apply to subsystem      " begin
+        @testset "Square op applied to ψ " begin
+            model = JuMP.Model()
+            H = [1 1; 1 -1]
+            JuMP.@variable(model, ψ[1:4])
+            res = Array{JuMP.AbstractJuMPScalar}(undef, 4)
+            res[1:2] .= H * ψ[1:2]
+            res[3:4] .= H * ψ[3:4]
+            @test apply_to_subsystem(H, ψ, 2, [2, 2]) == res
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                a = randn(T, 2, 2)
+                b = randn(T, 2, 2)
+                c = randn(T, 3, 3)
+                d = randn(T, 1, 1)
+                v1 = randn(T, 2)
+                v2 = randn(T, 2)
+                v3 = randn(T, 3)
+                ab = kron(a, b)
+                ac = kron(a, c)
+                bc = kron(b, c)
+                abc = kron(ab, c)
+                v12 = kron(v1, v2)
+                v13 = kron(v1, v3)
+                v23 = kron(v2, v3)
+                v123 = kron(v1, v2, v3)
+                I2 = Matrix(one(T) * I, (2, 2))
+                I3 = Matrix(one(T) * I, (3, 3))
+                I4 = Matrix(one(T) * I, (4, 4))
+                I6 = Matrix(one(T) * I, (6, 6))
+                @test apply_to_subsystem(a, v12, 1) ≈ kron(a, I2) * v12
+                @test apply_to_subsystem(a, v12, 2) ≈ kron(I2, a) * v12
+                @test apply_to_subsystem(a, v123, 1, [2, 2, 3]) ≈ kron(a, I6) * v123
+                @test apply_to_subsystem(a, v123, 2, [2, 2, 3]) ≈ kron(I2, a, I3) * v123
+                @test apply_to_subsystem(c, v123, 3, [2, 2, 3]) ≈ kron(I4, c) * v123
+                @test apply_to_subsystem(ab, v12, [1, 2]) ≈ ab * v12
+                @test apply_to_subsystem(ab, v12, [2, 1]) ≈ permute_systems(ab, [2, 1], [2, 2]) * v12
+                @test apply_to_subsystem(ac, v123, [2, 3], [2, 2, 3]) ≈ kron(I2, ac) * v123
+                @test apply_to_subsystem(bc, v123, [1, 3], [2, 2, 3]) ≈
+                      permute_systems(kron(I2, bc), [2, 1, 3], [2, 2, 3]) * v123
+                @test apply_to_subsystem(abc, v123, [2, 1, 3], [2, 2, 3]) ≈
+                      permute_systems(abc, [2, 1, 3], [2, 2, 3]) * v123
+                @test apply_to_subsystem(d, v123, 3, [2, 2, 1, 3]) ≈ v123 .* d[1, 1]
+            end
+            for R ∈ (Float64, BigFloat), (T, S) ∈ [(R, Complex{R}), (Complex{R}, R)]
+                I2 = Matrix(one(T) * I, (2, 2))
+                op = randn(T, 6, 6)
+                v = randn(S, 12)
+                @test apply_to_subsystem(op, v, [2, 3], [2, 2, 3]) ≈ kron(I2, op) * v
+            end
+        end
+        @testset "Rectangular op applied to ψ " begin
+            model = JuMP.Model()
+            H = [1 1; 1 -1]
+            JuMP.@variable(model, ψ[1:4])
+            res = Array{JuMP.AbstractJuMPScalar}(undef, 4)
+            res[1:2] .= H * ψ[1:2]
+            res[3:4] .= H * ψ[3:4]
+            @test apply_to_subsystem(H, ψ, 2, [2, 2]) == res
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                a = randn(T, 3, 2)
+                b = randn(T, 2, 3)
+                c = randn(T, 4, 6)
+                d = randn(T, 6, 4)
+                e = randn(T, 1, 4)
+                f = randn(T, 4, 1)
+                v2 = randn(T, 2)
+                v3 = randn(T, 3)
+                v4 = randn(T, 4)
+                v6 = randn(T, 6)
+                v12 = randn(T, 12)
+                v16 = randn(T, 16)
+                I2 = Matrix(one(T) * I, (2, 2))
+                @test apply_to_subsystem(a, v2, 1, [2]) ≈ a * v2
+                @test apply_to_subsystem(a, v4, 2, [2, 2]) ≈ kron(I2, a) * v4
+                @test apply_to_subsystem(b, v6, 2, [2, 3]) ≈ kron(I2, b) * v6
+                @test apply_to_subsystem(c, v12, [2, 3], [2, 2, 3]) ≈ kron(I2, c) * v12
+                @test apply_to_subsystem(d, v16, [2, 3], [2, 2, 2, 2]) ≈ kron(I2, d, I2) * v16
+                @test apply_to_subsystem(e, v16, [2, 3], [2, 2, 2, 2]) ≈ kron(I2, e, I2) * v16
+                @test apply_to_subsystem(f, v4, [2], [2, 1, 2]) ≈ kron(I2, f, I2) * v4
+            end
+            for R ∈ (Float64, BigFloat), (T, S) ∈ [(R, Complex{R}), (Complex{R}, R)]
+                I2 = Matrix(one(T) * I, (2, 2))
+                op = randn(T, 7, 6)
+                v = randn(S, 12)
+                @test apply_to_subsystem(op, v, [2, 3], [2, 2, 3]) ≈ kron(I2, op) * v
+            end
+        end
+        @testset "Square Kraus applied to ρ      " begin
+            model = JuMP.Model()
+            H = [1 1; 1 -1]
+            JuMP.@variable(model, ρ[1:4, 1:4], Hermitian)
+            res = Array{eltype(ρ)}(undef, 4, 4)
+            res[1:2, 1:2] = H * ρ[1:2, 1:2] * H'
+            res[1:2, 3:4] = H * ρ[1:2, 3:4] * H'
+            res[3:4, 1:2] = H * ρ[3:4, 1:2] * H'
+            res[3:4, 3:4] = H * ρ[3:4, 3:4] * H'
+            @test apply_to_subsystem([H], ρ, 2, [2, 2]) == res
+            d1, d2, d3 = 2, 2, 3
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                a = randn(T, d1, d1)
+                b = randn(T, d2, d2)
+                c = randn(T, d3, d3)
+                d = randn(T, 1, 1)
+                ab = kron(a, b)
+                ac = kron(a, c)
+                bc = kron(b, c)
+                abc = kron(ab, c)
+                I2 = Matrix(one(T) * I, (2, 2))
+                I3 = Matrix(one(T) * I, (3, 3))
+                I4 = Matrix(one(T) * I, (4, 4))
+                I6 = Matrix(one(T) * I, (6, 6))
+                @test apply_to_subsystem([a], ab, 1) ≈ kron(a, I2) * ab * kron(a, I2)'
+                @test apply_to_subsystem([a], ab, 2) ≈ kron(I2, a) * ab * kron(I2, a)'
+                @test apply_to_subsystem([a], abc, 1, [2, 2, 3]) ≈ kron(a, I6) * abc * kron(a, I6)'
+                @test apply_to_subsystem([a, b], abc, 2, [2, 2, 3]) ≈
+                      kron(I2, a, I3) * abc * kron(I2, a, I3)' + kron(I2, b, I3) * abc * kron(I2, b, I3)'
+                @test apply_to_subsystem([c], abc, 3, [2, 2, 3]) ≈ kron(I4, c) * abc * kron(I4, c)'
+                @test apply_to_subsystem([ab], ab, [1, 2]) ≈ ab * ab * ab'
+                @test apply_to_subsystem([ab], ab, [2, 1]) ≈
+                      permute_systems(ab, [2, 1], [2, 2]) * ab * permute_systems(ab, [2, 1], [2, 2])'
+                @test apply_to_subsystem([ac, bc], abc, [2, 3], [2, 2, 3]) ≈
+                      kron(I2, ac) * abc * kron(I2, ac)' + kron(I2, bc) * abc * kron(I2, bc)'
+                @test apply_to_subsystem([abc], abc, [2, 1, 3], [2, 2, 3]) ≈
+                      permute_systems(abc, [2, 1, 3], [2, 2, 3]) * abc * permute_systems(abc, [2, 1, 3], [2, 2, 3])'
+                @test apply_to_subsystem([d], abc, [1], [1, 2, 2, 3]) ≈ d[1, 1] .* abc .* d[1, 1]'
+                #sparse arrays
+                d = 3^4
+                SparseM = SparseArrays.spdiagm(-1 => randn(T, d - 1), 1 => randn(T, d - 1))
+                StdM = Matrix(SparseM)
+                op1 = randn(T, 3^2, 3^2)
+                op2 = randn(T, d, d)
+                @test apply_to_subsystem([op1], SparseM, [3, 1], [3, 3, 3, 3]) ≈
+                      apply_to_subsystem([op1], StdM, [3, 1], [3, 3, 3, 3])
+                @test apply_to_subsystem([op2], SparseM, [3, 1, 4, 2], [3, 3, 3, 3]) ≈
+                      apply_to_subsystem([op2], StdM, [3, 1, 4, 2], [3, 3, 3, 3])
+            end
+            for wrapper ∈ (Symmetric, Hermitian)
+                a = randn(ComplexF64, d1, d1)
+                b = randn(ComplexF64, d1 * d3, d1 * d3)
+                M = wrapper(randn(ComplexF64, (d1 * d2 * d3, d1 * d2 * d3)))
+                x = Matrix(M)
+                @test apply_to_subsystem([a], M, 1, [2, 2, 3]) ≈ apply_to_subsystem([a], x, 1, [2, 2, 3])
+                @test apply_to_subsystem([b], M, [1, 3], [2, 2, 3]) ≈ apply_to_subsystem([b], x, [1, 3], [2, 2, 3])
+            end
+            for R ∈ (Float64, BigFloat), (T, S) ∈ [(R, Complex{R}), (Complex{R}, R)]
+                I2 = Matrix(one(T) * I, (2, 2))
+                k = randn(T, 6, 6)
+                ρ = randn(S, 12, 12)
+                @test apply_to_subsystem([k], ρ, [2, 3], [2, 2, 3]) ≈ kron(I2, k) * ρ * kron(I2, k)'
+            end
+        end
+        @testset "Rectangular Kraus applied to ρ       " begin
+            model = JuMP.Model()
+            k = [1 1; 1 -1; 1 -1]
+            JuMP.@variable(model, ρ[1:4, 1:4], Hermitian)
+            res = Array{eltype(ρ)}(undef, 6, 6)
+            res[1:3, 1:3] = k * ρ[1:2, 1:2] * k'
+            res[1:3, 4:6] = k * ρ[1:2, 3:4] * k'
+            res[4:6, 1:3] = k * ρ[3:4, 1:2] * k'
+            res[4:6, 4:6] = k * ρ[3:4, 3:4] * k'
+            @test apply_to_subsystem([k], ρ, 2, [2, 2]) == res
+            d1, d2 = 2, 3
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                k1 = randn(T, d1, d2)
+                k2 = randn(T, d1, d2)
+                k3 = randn(T, d2, d1)
+                k4 = randn(T, d2, d1)
+                k5 = randn(T, d1 * d1, d2 * d2)
+                k6 = randn(T, d2 * d2, d1 * d1)
+                k7 = randn(T, 1, d1 * d1)
+                k8 = randn(T, d1, 1)
+
+                a = randn(T, d1^3, d1^3)
+                b = randn(T, d2^3, d2^3)
+                c = randn(T, 3, 3)
+
+                I2 = Matrix(one(T) * I, (2, 2))
+                I3 = Matrix(one(T) * I, (3, 3))
+                I4 = Matrix(one(T) * I, (4, 4))
+                I9 = Matrix(one(T) * I, (9, 9))
+                @test apply_to_subsystem([k1, k2], c, 1, [3]) ≈ k1 * c * k1' + k2 * c * k2'
+                @test apply_to_subsystem([k1, k2], b, 1, [3, 3, 3]) ≈
+                      kron(k1, I9) * b * kron(k1, I9)' + kron(k2, I9) * b * kron(k2, I9)'
+                @test apply_to_subsystem([k1, k2], b, 3, [3, 3, 3]) ≈
+                      kron(I9, k1) * b * kron(I9, k1)' + kron(I9, k2) * b * kron(I9, k2)'
+                @test apply_to_subsystem([k1, k2], b, 2, [3, 3, 3]) ≈
+                      permute_systems(kron(k1, I9), [2, 1, 3], [2 3; 3 3; 3 3]) *
+                      b *
+                      permute_systems(kron(k1, I9), [2, 1, 3], [2 3; 3 3; 3 3])' +
+                      permute_systems(kron(k2, I9), [2, 1, 3], [2 3; 3 3; 3 3]) *
+                      b *
+                      permute_systems(kron(k2, I9), [2, 1, 3], [2 3; 3 3; 3 3])'
+
+                @test apply_to_subsystem([k5], b, [1, 2], [3, 3, 3]) ≈ kron(k5, I3) * b * kron(k5, I3)'
+                @test apply_to_subsystem([k6], a, [2, 3], [2, 2, 2]) ≈ kron(I2, k6) * a * kron(I2, k6)'
+                @test apply_to_subsystem([k7], a, [2, 3], [2, 2, 2]) ≈ kron(I2, k7) * a * kron(I2, k7)'
+                @test apply_to_subsystem([k8], a, 2, [2, 1, 2, 2]) ≈ kron(I2, k8, I4) * a * kron(I2, k8, I4)'
+
+                #sparse arrays
+                d = 3^4
+                SparseM = SparseArrays.spdiagm(-1 => randn(T, d - 1), 1 => randn(T, d - 1))
+                StdM = Matrix(SparseM)
+                k1 = sparse(randn(T, 2^2, 3^2))
+                k2 = sparse(randn(T, 2^2, 3^2))
+                k3 = sparse(randn(T, 1, 3^2))
+                k4 = sparse(randn(T, 1, 3^2))
+                @test apply_to_subsystem([k1, k2], SparseM, [2, 3], [3, 3, 3, 3]) ≈
+                      apply_to_subsystem([k1, k2], StdM, [2, 3], [3, 3, 3, 3])
+                @test apply_to_subsystem([k3, k4], SparseM, [2, 3], [3, 3, 3, 3]) ≈
+                      apply_to_subsystem([k3, k4], StdM, [2, 3], [3, 3, 3, 3])
+            end
+            for R ∈ (Float64, BigFloat), (T, S) ∈ [(R, Complex{R}), (Complex{R}, R)]
+                I2 = Matrix(one(T) * I, (2, 2))
+                k = randn(T, 7, 6)
+                ρ = randn(S, 12, 12)
+                @test apply_to_subsystem([k], ρ, [2, 3], [2, 2, 3]) ≈ kron(I2, k) * ρ * kron(I2, k)'
+
+                k_sparse = sparse(k)
+                Sparse_ρ = SparseArrays.spdiagm(-1 => randn(S, 12 - 1), 1 => randn(S, 12 - 1))
+                @test apply_to_subsystem([k_sparse], Sparse_ρ, [2, 3], [2, 2, 3]) ≈
+                      apply_to_subsystem([Matrix(k_sparse)], Matrix(Sparse_ρ), [2, 3], [2, 2, 3])
+            end
+        end
+    end
     @testset "Partial trace      " begin
         model = JuMP.Model()
         JuMP.@variable(model, ρ[1:4, 1:4], Symmetric)
@@ -94,6 +318,9 @@
                 @test permute_systems(uvw, [1, 3, 2], [d1, d2, d3]) ≈ kron(u, w, v)
                 @test permute_systems(uvw, [2, 1, 3], [d1, d2, d3]) ≈ kron(v, u, w)
                 @test permute_systems(uvw, [3, 2, 1], [d1, d2, d3]) ≈ kron(w, v, u)
+
+                @test permute_systems(uvw, [5, 4, 3, 2, 1], [d1, 1, d2, 1, d3]) ≈ kron(w, v, u)
+                @test permute_systems(uvw, [3, 4, 5, 2, 1, 6], [d1, d2, d3, 1, 1, 1]) ≈ kron(w, v, u)
             end
         end
 
@@ -176,7 +403,8 @@
             0 tr(ρ[3:4, 1:2])./2 0 tr(ρ[3:4, 3:4])./2
         ]
         @test trace_replace(ρ, 2, [2, 2]) == trrp
-        a = ones(Int, 4, 4); a[1] = 2
+        a = ones(Int, 4, 4)
+        a[1] = 2
         trrp = [
             3/2 0 1 0
             0 3/2 0 1
@@ -221,5 +449,3 @@
         end
     end
 end
-
-#TODO add test with JuMP variables
