3971b8593b7981d4eebdba43a13968af761073e3
AVlyx <anton@vancleem.com>
2025-05-04 17:34:56 +0200

    permute systems tests

--- test/multilinear.jl
+++ test/multilinear.jl
@@ -298,6 +298,101 @@
             @test partial_transpose(M, [1, 3], [d1, d2, d3]) ≈ partial_transpose(x, [1, 3], [d1, d2, d3])
         end
     end
+
+    @testset "Permute systems    " begin
+        @testset "Vectors" begin
+            d1, d2, d3 = 2, 2, 3
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                u = randn(T, d1)
+                v = randn(T, d2)
+                w = randn(T, d3)
+                uv = kron(u, v)
+                vw = kron(v, w)
+                uvw = kron(u, v, w)
+                @test permute_systems(uv, [1, 2]) ≈ kron(u, v)
+                @test permute_systems(uv, [2, 1]) ≈ kron(v, u)
+                @test permute_systems(vw, [2, 1], [d2, d3]) ≈ kron(w, v)
+                @test permute_systems(uvw, [1, 2, 3], [d1, d2, d3]) ≈ kron(u, v, w)
+                @test permute_systems(uvw, [2, 3, 1], [d1, d2, d3]) ≈ kron(v, w, u)
+                @test permute_systems(uvw, [3, 1, 2], [d1, d2, d3]) ≈ kron(w, u, v)
+                @test permute_systems(uvw, [1, 3, 2], [d1, d2, d3]) ≈ kron(u, w, v)
+                @test permute_systems(uvw, [2, 1, 3], [d1, d2, d3]) ≈ kron(v, u, w)
+                @test permute_systems(uvw, [3, 2, 1], [d1, d2, d3]) ≈ kron(w, v, u)
+
+                @test permute_systems(uvw, [5, 4, 3, 2, 1], [d1, 1, d2, 1, d3]) ≈ kron(w, v, u)
+                @test permute_systems(uvw, [3, 4, 5, 2, 1, 6], [d1, d2, d3, 1, 1, 1]) ≈ kron(w, v, u)
+            end
+        end
+
+        @testset "Square matrices" begin
+            d1, d2, d3 = 2, 2, 3
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                a = randn(T, d1, d1)
+                b = randn(T, d2, d2)
+                c = randn(T, d3, d3)
+                ab = kron(a, b)
+                ac = kron(a, c)
+                bc = kron(b, c)
+                abc = kron(a, b, c)
+                @test permute_systems(ab, [1, 2]) ≈ kron(a, b)
+                @test permute_systems(ab, [2, 1]) ≈ kron(b, a)
+                @test permute_systems(ac, [2, 1], [d1, d3]) ≈ kron(c, a)
+                @test permute_systems(bc, [2, 1], [d2, d3]) ≈ kron(c, b)
+                @test permute_systems(abc, [1, 2, 3], [d1, d2, d3]) ≈ kron(a, b, c)
+                @test permute_systems(abc, [2, 3, 1], [d1, d2, d3]) ≈ kron(b, c, a)
+                @test permute_systems(abc, [3, 1, 2], [d1, d2, d3]) ≈ kron(c, a, b)
+                @test permute_systems(abc, [1, 3, 2], [d1, d2, d3]) ≈ kron(a, c, b)
+                @test permute_systems(abc, [2, 1, 3], [d1, d2, d3]) ≈ kron(b, a, c)
+                @test permute_systems(abc, [3, 2, 1], [d1, d2, d3]) ≈ kron(c, b, a)
+
+                p = permutation_matrix(d1, [1, 2])
+                @test permute_systems(ab, [1, 2]) ≈ p * ab * p'
+                p = permutation_matrix([d1, d2], [2, 1])
+                @test permute_systems(ab, [2, 1]) ≈ p * ab * p'
+                p = permutation_matrix([d1, d2, d3], [1, 2, 3])
+                @test permute_systems(abc, [1, 2, 3], [d1, d2, d3]) ≈ p * abc * p'
+                p = permutation_matrix([d1, d2, d3], [2, 3, 1])
+                @test permute_systems(abc, [2, 3, 1], [d1, d2, d3]) ≈ p * abc * p'
+                p = permutation_matrix([d1, d2, d3], [3, 1, 2])
+                @test permute_systems(abc, [3, 1, 2], [d1, d2, d3]) ≈ p * abc * p'
+                p = permutation_matrix([d1, d2, d3], [1, 3, 2])
+                @test permute_systems(abc, [1, 3, 2], [d1, d2, d3]) ≈ p * abc * p'
+                p = permutation_matrix([d1, d2, d3], [2, 1, 3])
+                @test permute_systems(abc, [2, 1, 3], [d1, d2, d3]) ≈ p * abc * p'
+                p = permutation_matrix([d1, d2, d3], [3, 2, 1])
+                @test permute_systems(abc, [3, 2, 1], [d1, d2, d3]) ≈ p * abc * p'
+            end
+            for wrapper ∈ (Symmetric, Hermitian)
+                M = wrapper(randn(ComplexF64, (d1 * d2 * d3, d1 * d2 * d3)))
+                x = Matrix(M)
+                @test permute_systems(M, [3, 1, 2], [d1, d2, d3]) ≈ permute_systems(x, [3, 1, 2], [d1, d2, d3])
+                @test permute_systems(M, [1, 3, 2], [d1, d2, d3]) ≈ permute_systems(x, [1, 3, 2], [d1, d2, d3])
+            end
+        end
+
+        @testset "Rectangular matrices" begin
+            d1, d2, d3 = 2, 3, 4
+            for R ∈ (Float64, BigFloat), T ∈ (R, Complex{R})
+                a = randn(T, d1, d2)
+                b = randn(T, d1, d3)
+                c = randn(T, d2, d3)
+                ab = kron(a, b)
+                ac = kron(a, c)
+                bc = kron(b, c)
+                abc = kron(a, b, c)
+                @test permute_systems(ab, [1, 2], [d1 d2; d1 d3]) ≈ kron(a, b)
+                @test permute_systems(ab, [2, 1], [d1 d2; d1 d3]) ≈ kron(b, a)
+                @test permute_systems(ac, [2, 1], [d1 d2; d2 d3]) ≈ kron(c, a)
+                @test permute_systems(bc, [2, 1], [d1 d3; d2 d3]) ≈ kron(c, b)
+                @test permute_systems(abc, [1, 2, 3], [d1 d2; d1 d3; d2 d3]) ≈ kron(a, b, c)
+                @test permute_systems(abc, [2, 3, 1], [d1 d2; d1 d3; d2 d3]) ≈ kron(b, c, a)
+                @test permute_systems(abc, [3, 1, 2], [d1 d2; d1 d3; d2 d3]) ≈ kron(c, a, b)
+                @test permute_systems(abc, [1, 3, 2], [d1 d2; d1 d3; d2 d3]) ≈ kron(a, c, b)
+                @test permute_systems(abc, [2, 1, 3], [d1 d2; d1 d3; d2 d3]) ≈ kron(b, a, c)
+                @test permute_systems(abc, [3, 2, 1], [d1 d2; d1 d3; d2 d3]) ≈ kron(c, b, a)
+            end
+        end
+    end
     @testset "Trace replace      " begin
         model = JuMP.Model()
         JuMP.@variable(model, ρ[1:4, 1:4], Hermitian)
