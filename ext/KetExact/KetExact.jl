3d2e1ab7751795db8a2a498221a8311d8abfa619
Sébastien Designolle <sebastien.designolle@gmail.com>
2025-11-04 09:22:30 +0100

    Minor

--- ext/KetExact/KetExact.jl
+++ ext/KetExact/KetExact.jl
@@ -6,12 +6,12 @@ import LinearAlgebra as LA
 
 Base.complex(::Type{CN.Cyc{R}}) where {R<:Real} = CN.Cyc{R}
 
-Ket._root_unity(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.E(Int64(n))
-Ket._sqrt(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.root(Int64(n))
+Ket._root_unity(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.E(Int(n))
+Ket._sqrt(::Type{CN.Cyc{R}}, n::Integer) where {R<:Real} = CN.root(Int(n))
 Ket._rtol(::Type{CN.Cyc{R}}) where {R<:Real} = sqrt(_eps(R))
 Ket._eps(::Type{CN.Cyc{R}}) where {R<:Real} = R(0)
 Ket._eps(::Type{CN.Cyc{R}}) where {R<:AbstractFloat} = eps(R)
 
-LA.norm(v::AbstractVector{CN.Cyc{R}}) where {R<:Real} = Ket._sqrt(CN.Cyc{R}, Int64(sum(abs2, v)))
+LA.norm(v::AbstractVector{CN.Cyc{R}}) where {R<:Real} = Ket._sqrt(CN.Cyc{R}, Int(sum(abs2, v)))
 
 end # module
