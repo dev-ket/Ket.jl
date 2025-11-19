77e46b81786736cc4fdb930d72702d4f125d907f
Sébastien Designolle <sebastien.designolle@gmail.com>
2025-11-19 14:48:13 +0100

    Polish docstrings

--- src/nonlocal.jl
+++ src/nonlocal.jl
@@ -268,8 +268,9 @@ end
 """
     tensor_collinsgisin(p::Array, behaviour::Bool = false; correlation::Bool = false)
 
-Converts a multipartite Bell functional `p` into Collins-Gisin notation.
-If `correlation` is `true`, `p` is assumed to be written in correlation notation. Otherwise, `p` is assumed to be written in probability notation.
+Converts a multipartite Bell functional `p` into Collins-Gisin notation, with marginals in the first index of each dimension.
+If `correlation` is `true`, `p` is assumed to be written in correlation notation.
+Otherwise, `p` is assumed to be written in probability notation.
 If `behaviour` is `true` do instead the transformation for behaviours. Doesn't assume normalization.
 
 Also accepts the arguments of `tensor_probability` (state and measurements) for convenience.
